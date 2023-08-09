a8abae4fd8e1585b8194c3a3bee552b0746ad98f
Jim Lu <jimlue123@gmail.com>
2023-08-09 19:26:33 +0800

    First Commit

---
+++ ElevatorTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace ElevatorTest;
+using Xunit;
+using ElevatorSimulator;
+
+public class ElevatorTests
+{
+    [Fact]
+    public void TestElevatorMove()
+    {
+        // Arrange
+        Elevator e = new Elevator("TestElevator", 1);
+
+        // Act
+        e.Move(5);
+
+        // Assert
+        Assert.Equal(5, e.current_floor);
+    }
+}
